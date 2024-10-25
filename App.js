61bf41cd31dc9631142eeb055f8a7799b7f99255
KhanhTran <khanh.euroland@gmail.com>
2024-10-25 10:03:46 +0700

    implement redux persist

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import PersonScreen from './screen/PersonScreen';
-import HomeScreen from './screen/HomeScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import PersonScreen from "./screen/PersonScreen";
+import HomeScreen from "./screen/HomeScreen";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
   return (
     //Use Redux
-    /*
+
     <Provider store={store}>
-      <PersonScreen/>
+      {/*PersistGate dam bao store dc khoi phuc tu storage truoc khi UI dc render*/
+      /*Persist gate tri hoan render UI cho den khi redux store dc khoi phuc hoan toan */
+      }
+      
+      <PersistGate persistor={persistor}>
+        <PersonScreen />
+      </PersistGate>
     </Provider>
-    */
 
     //Not Redux
-    <HomeScreen></HomeScreen>
+    //<HomeScreen></HomeScreen>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from "redux-persist";
 import personReducer from './personSlice'
 import authReducer from "./authSlice";
 
+//Cau hinh redux-persist
+const persistConfig = {
+    key: 'root', //any content
+    storage: AsyncStorage, //Su dung async storage de lu du lieu
+    whitelist: ['person', 'auth'] //Danh sach cac reducer muon luu data vao storage
+}
+
+//Combine reducers 
+const rootReducer = combineReducers({
+    person: personReducer,
+    auth: authReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+//Cau hinh store voi persistedReducer
 export const store = configureStore({
-    reducer: {
-        person: personReducer,
-        auth: authReducer
-    }
+    reducer: persistedReducer
 });
+
+export const persistor = persistStore(store); //Taoj persistor
